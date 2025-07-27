70bf4beb683c8264b3e3ce158ebd3bb5a8edff82
Silas Pascoal <silas.pascoal@onfly.com.br>
2025-07-27 12:36:30 -0300

    bloqueio básico ao dashboard

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,4 +1,13 @@
-export default function Dashboard() {
+import getSession from '@/lib/getSession'
+import { redirect } from 'next/navigation';
+
+export default async function Dashboard() {
+  const session = await getSession()
+
+  if (!session) {
+    redirect('/');
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
