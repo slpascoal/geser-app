49a47c33b33278a546dbbb2a43ea2f2cefeefeb3
Silas Pascoal <silas.pascoal@onfly.com.br>
2025-07-27 07:51:49 -0300

    criando método provisório de login com github

--- src/app/(public)/_components/navLinks.tsx
+++ src/app/(public)/_components/navLinks.tsx
@@ -1,49 +1,62 @@
 import { Button } from "@/components/ui/button";
-import { LogIn } from "lucide-react";
+import { Loader2Icon, LogIn } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { login } from "../_actions/login";
 
 type Props = {
   setIsOpen?: () => void;
 };
 
 export function NavLinks({ setIsOpen }: Props) {
-    const navItems = [
-        { label: "Profissionais", href: "#profissionais" }
-      ];
+  const {data: session, status} = useSession();
+  
+  async function handleLogin() {
+    await login("github");
+  }
 
-    const session = null;
-    
-    return (
-      <>
-      {navItems.map((item) => (
-        <Button
-          onClick={setIsOpen}
-          key={item.href}
-          asChild
-          className="text-black bg-transparent hover:bg-transparent shadow-none"
+  const navItems = [
+      { label: "Profissionais", href: "#profissionais" }
+    ];
+  
+  return (
+    <>
+    {navItems.map((item) => (
+      <Button
+        onClick={setIsOpen}
+        key={item.href}
+        asChild
+        className="text-black bg-transparent hover:bg-transparent shadow-none"
+      >
+        <Link 
+          href={item.href}
+          className="text-base"
         >
-          <Link 
-            href={item.href}
-            className="text-base"
-          >
-            {item.label}
-          </Link>
-        </Button>
-      ))}
-
-      {session ? (
-        <Link
-          href="/dashboard"
-          className="flex items-center justify-center gap-2"
-        >
-          Dashboard
+          {item.label}
         </Link>
-      ) : (
-        <Button>
-          <LogIn />
-          Login
-        </Button>
-      )}  
-      </>
-    )
+      </Button>
+    ))}
+
+    {status === 'loading' ? (
+      <Button disabled>
+        <Loader2Icon className="animate-spin" />
+      </Button>
+    ) : session ? (
+      <Link
+        href="/dashboard"
+        className="flex items-center justify-center gap-2 bg-zinc-900 text-white py-1 px-4 rounded-md"
+      >
+        Dashboard
+      </Link>
+    ) : (
+      <Button 
+        className="cursor-pointer"
+        onClick={handleLogin}
+      >
+        <LogIn />
+        Login
+      </Button>
+    )}  
+    </>
+  )
 }
